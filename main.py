cc781c0d7bda780ab3f2e8c25993634591307cbc
EC2 Default User <ec2-user@ip-172-31-7-26.ec2.internal>
2021-02-07 00:34:27 +0000

    gained server-to-server authentication

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 from flask import Flask, render_template
 import requests
+from dotenv import load_dotenv, find_dotenv
 import os
+import json
 
-response = requests.get('https://api.spotify.com/v1/artists/{1gUi2utSbJLNPddYENJAp4}/top-tracks')
+#Authorization for spotify API
+
 AUTH_URL = 'https://accounts.spotify.com/api/token'
 CLIENT_ID = '9a99f0534e6a442581f56d58b1a97afa'
+
+load_dotenv(find_dotenv())
+
+auth_response = requests.post(AUTH_URL, {
+    
+    'grant_type' : 'client_credentials',
+    'client_id' : CLIENT_ID,
+    'client_secret' : os.getenv('SPT_KEY'), 
+})
+
+auth_response_data = auth_response.json()
+access_token = auth_response_data['access_token']
+
+headers = {
+    'Authorization' : 'Bearer {token}'.format(token=access_token)
+    }
+    
+BASE_URL = 'https://api.spotify.com/v1/browse/new-releases'
+country = 'US'
+
+response = requests.get(BASE_URL + "?country=" + country, headers=headers)
+data = response.json()
 
 app = Flask(__name__)
 
 @app.route('/')
 def spotify_list():
-    return render_template('Spotify.html')
+    return render_template('Spotify.html', json_data = data)
 
-data = response.json()
-
-print(data)
 
 app.run(port=int(os.getenv('PORT', 8080)), host=os.getenv('IP', '0.0.0.0'))
