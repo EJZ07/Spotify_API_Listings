29622fac59474b5ffb206d0f04f19a4c07dcc4e6
EC2 Default User <ec2-user@ip-172-31-7-26.ec2.internal>
2021-02-14 01:15:05 +0000

    Added Lyrics for random song

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ ARTIST_URL = 'https://api.spotify.com/v1/artists?ids='
 TOP_TRACKS = 'https://api.spotify.com/v1/artists/'
 
 the_weekend = '1Xyo4u8uXC1ZmMpatF05PJ'
-birocratic = '60b7IDlGflg5lgyfEGf9yB'
+mystery = '5ZbRDyTYX4HhXveONcZQn2'
 tyler = '4V8LLVI7PbaPR0K2TGSxFF'
 
 market = "US"
@@ -41,24 +41,27 @@ market = "US"
 response = requests.get(TRACK_URL + '62PclpoBFLl50PJpiJA5RT', headers=headers)
 data = response.json()
 
-response0 = requests.get(ARTIST_URL + the_weekend + "," + birocratic + "," + tyler, headers=headers)
+response0 = requests.get(ARTIST_URL + the_weekend + "," + mystery + "," + tyler, headers=headers)
 data0 = response0.json()
 
 num = random.randint(0,2)
 if num == 0:
     artist_info = the_weekend
 elif num == 1:
-    artist_info = birocratic
+    artist_info = mystery
 elif num == 2:
     artist_info = tyler
     
 response1 = requests.get(TOP_TRACKS + artist_info + "/top-tracks?" + "market=" + market, headers=headers)
 data1 = response1.json()
 
+rand_track = random.randint(0, len(data1['tracks']))
+
+
 headers = {
     'Authorization' : 'Bearer ' + os.getenv('G_ACCESS_TOKEN')
     }
-response_alt = requests.get(SEARCH_URL + data1['tracks'][0]['name'], headers=headers)
+response_alt = requests.get(SEARCH_URL + data1['tracks'][rand_track]['name'], headers=headers)
 data_alt = response_alt.json()
 response2 = requests.get(LYRICS_URL + str(data_alt['response']['hits'][0]['result']['id']), headers=headers)
 data2 = response2.json()
@@ -69,7 +72,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def spotify_list():
-    return render_template('Spotify.html', json_data = data, json_data0 = data0, json_data1 = data1, json_data2 = data2)
+    return render_template('Spotify.html', json_data = data, json_data0 = data0, json_data1 = data1, g_song = data2)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.run(port=int(os.getenv('PORT', 8080)), host=os.getenv('IP', '0.0.0.0'))
