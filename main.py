29c5c5c5914b873fbb58eca4d5972bb7fb673b51
EC2 Default User <ec2-user@ip-172-31-7-26.ec2.internal>
2021-02-15 02:41:16 +0000

    Changes to interface

--- main.py
+++ main.py
@@ -28,27 +28,24 @@ headers = {
     
 SEARCH_URL = 'http://api.genius.com/search?q='
 LYRICS_URL = 'http://api.genius.com/songs/'
-TRACK_URL = 'https://api.spotify.com/v1/tracks/'
+TRACK_URL =  'https://api.spotify.com/v1/search?q='
 ARTIST_URL = 'https://api.spotify.com/v1/artists?ids='
 TOP_TRACKS = 'https://api.spotify.com/v1/artists/'
 
 the_weekend = '1Xyo4u8uXC1ZmMpatF05PJ'
-mystery = '5ZbRDyTYX4HhXveONcZQn2'
+tobi = '4T8NIfZmVY6TJFqVzN6X49'
 tyler = '4V8LLVI7PbaPR0K2TGSxFF'
 
 market = "US"
 
-response = requests.get(TRACK_URL + '62PclpoBFLl50PJpiJA5RT', headers=headers) #ThunderCat track URL
-data = response.json()
-
-response0 = requests.get(ARTIST_URL + the_weekend + "," + mystery + "," + tyler, headers=headers)
+response0 = requests.get(ARTIST_URL + the_weekend + "," + tobi + "," + tyler, headers=headers)
 data0 = response0.json()
 
 num = random.randint(0,2)
 if num == 0:
     artist_info = the_weekend
 elif num == 1:
-    artist_info = mystery
+    artist_info = tobi
 elif num == 2:
     artist_info = tyler
     
@@ -56,6 +53,9 @@ response1 = requests.get(TOP_TRACKS + artist_info + "/top-tracks?" + "market=" +
 data1 = response1.json()
 
 rand_track = random.randint(0, len(data1['tracks'])-1)
+
+response_p = requests.get(TRACK_URL + data1['tracks'][rand_track]['name'] + "&type=track", headers=headers)
+data_p = response_p.json()
 
 headers = {
     'Authorization' : 'Bearer ' + os.getenv('G_ACCESS_TOKEN')
@@ -69,7 +69,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def spotify_list():
-    return render_template('Spotify.html', json_data = data, json_data0 = data0, json_data1 = data1, g_song = data2)
+    return render_template('Spotify.html', preview = data_p, json_data1 = data1, g_song = data2)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.run(port=int(os.getenv('PORT', 8080)), host=os.getenv('IP', '0.0.0.0'))
